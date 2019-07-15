8d80fc02d6514b416b291f547c8063e27d0e1c1a
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2019-07-14 22:41:43 -0500

    Add terrible state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
-function App() {
-  return <div>My App!</div>
+import ExpansionPanels from './ExpansionPanels'
+
+const N = 100
+const initalState = Array.from({ length: N }).map((_, i) => ({
+  id: i,
+  firstName: 'placeholder first name',
+  lastName: 'placeholder last name',
+  email: 'email@example.com'
+}))
+
+const reducer = (state, action) => {
+  const handleUpdateState = updateState(state)
+  switch (action.type) {
+    case setFirstName().type:
+      return handleUpdateState(action.id, 'firstName', action.firstName)
+
+    case setLastName().type:
+      return handleUpdateState(action.id, 'lastName', action.lastName)
+    case setEmail().type:
+      return handleUpdateState(action.id, 'email', action.email)
+
+    default:
+      return state
+  }
+}
+
+const updateState = items => (idToFind, attr = '', value = '') => {
+  const itemIndex = items.findIndex(({ id }) => id === idToFind)
+  // if item was not found, don't alter the state
+  if (itemIndex === -1) return items
+  const itemsCopy = [...items]
+  const newItem = { ...items[itemIndex], [attr]: value }
+  itemsCopy.splice(itemIndex, 1, newItem)
+  return itemsCopy
+}
+
+const setFirstName = (id = null, firstName = '') => ({
+  type: 'SET_FIRST_NAME',
+  id,
+  firstName
+})
+
+const setLastName = (id = null, lastName = '') => ({
+  type: 'SET_LAST_NAME',
+  id,
+  lastName
+})
+
+const setEmail = (id = null, email = '') => ({
+  type: 'SET_EMAIL',
+  id,
+  email
+})
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initalState)
+
+  const handleSetFirstName = (id, firstName) =>
+    dispatch(setFirstName(id, firstName))
+
+  const handleSetLastName = (id, lastName) =>
+    dispatch(setLastName(id, lastName))
+
+  const handleSetEmail = (id, email) => dispatch(setEmail(id, email))
+  return (
+    <div style={{ width: '50%', margin: '0 auto' }}>
+      <h1>Optimizing a React App</h1>
+      <ExpansionPanels
+        items={state}
+        setFirstName={handleSetFirstName}
+        setLastName={handleSetLastName}
+        setEmail={handleSetEmail}
+      />
+    </div>
+  )
 }
 
 export default App
---
+++ src/ExpansionPanels.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+
+import {
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Divider
+} from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+const ExpansionPanels = ({ items, ...handlers }) => {
+  return items.map(item => (
+    <CustomExpansionPanel key={item.id} {...item} {...handlers} />
+  ))
+}
+
+const CustomExpansionPanel = ({
+  id,
+  firstName,
+  lastName,
+  email,
+  setFirstName,
+  setLastName,
+  setEmail
+}) => {
+  const [expanded, setExpanded] = useState(true)
+
+  return (
+    <ExpansionPanel
+      expanded={expanded}
+      onChange={() => setExpanded(prevExpanded => !prevExpanded)}
+    >
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls={`panel${id}-content`}
+      >
+        <span>Form {id}</span>
+      </ExpansionPanelSummary>
+      <Divider />
+      <ExpansionPanelDetails>
+        <form onSubmit={e => e.preventDefault()}>
+          <span>First name:</span>
+          <br />
+          <input
+            type="text"
+            name="firstname"
+            value={firstName}
+            onChange={e => setFirstName(id, e.target.value)}
+          />
+          <br />
+          <span>Last name:</span>
+          <br />
+          <input
+            type="text"
+            name="lastname"
+            value={lastName}
+            onChange={e => setLastName(id, e.target.value)}
+          />
+          <br />
+          <span>email:</span>
+          <br />
+          <input
+            type="email"
+            name="lastname"
+            value={email}
+            onChange={e => setEmail(id, e.target.value)}
+          />
+        </form>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  )
+}
+
+export default ExpansionPanels
